27110b407e4dfecb02f709bfe0b50125f1a15fee
kiciahook <ziaquiq@gmail.com>
2025-11-22 00:37:19 +0100

    fix up

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 # Fun fact: you can run PowerShell scripts on Linux!
 
 pesde run sourcemap_generator | Out-File .meta/sourcemap.json -Encoding utf8
-stylua lib -f .meta/.stylua.toml
-selene lib --config .meta/selene.toml
-darklua process lib/init.lua dist.lua -c .meta/.darklua.json
+stylua src -f .meta/.stylua.toml
+selene src --config .meta/selene.toml
+darklua process src/init.luau dist.lua -c .meta/.darklua.json
